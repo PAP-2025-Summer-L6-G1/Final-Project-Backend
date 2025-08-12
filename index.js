279e7d953be8c9386fca98ed0b686060e7302063
= <=>
2025-08-12 12:33:30 -0700

    pushing to github

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const NutritionAPI = require("./api/nutrition");
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://localhost:5174", "https://localhost:5173", "https://localhost:5174", "https://cfa-summer2025-grocerybuddy-www.netlify.app"],
+        origin: ["http://localhost:5173", "http://localhost:5174", "https://localhost:5173", "https://cfa-summer2025-grocerybuddy-www.netlify.app"],
         credentials: true,
     })
 );
